0f5bdebd8919dd0ef3d54a999ff74aa4348e49e6
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-02-03 11:54:35 +0300

    add blank start page

--- affiche/urls.py
+++ affiche/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from affiche.views import show_homepage
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", show_homepage),
 ]
