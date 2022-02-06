7af94d653948f156b0a3235cf28cecdb1743496d
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-02-06 07:20:05 +0300

    add photo preview for places

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from places.models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ["place_preview"]
+    fields = ("img", "place_preview", "order")
+
+    def place_preview(self, obj):
+        return format_html(f'<img src="{obj.img.url}" height="200px" />')
 
 
 class PlaceAdmin(admin.ModelAdmin):
